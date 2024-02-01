d0afdc811424f5f50f240e3277d1eca957db6a11
Emre Deniz <emre.deniz@outlook.com>
2024-02-01 12:47:28 -0500

    UI: Contacts scrollable list, Load & Save Buttons

--- app/src/main/java/com/example/mapd721_a2_emredeniz/MainActivity.kt
+++ app/src/main/java/com/example/mapd721_a2_emredeniz/MainActivity.kt
@@ -11,11 +11,16 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -39,15 +44,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
-    // Define context, scope and datastore
+    // Define context
     val context = LocalContext.current
-    val scope = rememberCoroutineScope()
 
     Column(modifier = Modifier.fillMaxSize()) {
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        // Username field
+        // Contact Name field
         OutlinedTextField(
             modifier = Modifier.padding(start = 15.dp, end = 15.dp).fillMaxWidth(),
             value = "",
@@ -55,9 +59,12 @@ fun MainScreen() {
             label = { Text(text = "Contact Name", color = Color.Black, fontSize = 14.sp) }
         )
 
-        // Email field
+        // Contact Number field
         OutlinedTextField(
             modifier = Modifier.padding(start = 15.dp, end = 15.dp).fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Phone
+            ),
             value = "",
             onValueChange = { },
             label = { Text(text = "Contact No", color = Color.Black, fontSize = 14.sp) }
@@ -103,7 +110,42 @@ fun MainScreen() {
 
         }
 
-        Spacer(modifier = Modifier.height(400.dp))
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Text(
+            text = "CONTACTS",
+            color = Color.Black,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(start = 15.dp)
+        )
+
+        // Contacts list
+        Box(
+            modifier = Modifier
+                .padding(17.dp)
+                .border(BorderStroke(1.dp, Color.Black))
+                .background(Color(230, 230, 230))
+        ) {
+            Column(
+                modifier = Modifier
+                    .background(Color(230, 230, 230))
+                    .size(330.dp)
+                    .fillMaxWidth()
+                    .padding(15.dp)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                repeat(30) {
+                    Text(
+                        text = "Contact Name Number $it",
+                        fontSize = 18.sp,
+                        modifier = Modifier.padding(2.dp)
+                    )
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(30.dp))
 
         // About section
         Box(
